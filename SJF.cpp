72e73b72b6afd744b7e3d4f52807cbb0111c6c28
Knl-Bill <115896029+Knl-Bill@users.noreply.github.com>
2023-08-01 22:28:47 +0530

    Update SJF.cpp
    Added comments to the SJF.cpp file for the better understanding of the code.

--- SJF.cpp
+++ SJF.cpp
@@ -5,16 +5,20 @@
 using namespace std;
 int main()
 {
+    // The code is written assuming that the arrival time for all the processes is the same
     cout<<"Number of processes : ";
     int n,a,b,burst=0;
     cin>>n;
     vector<int> m(n);
+    // Using maps to take the input as the map keys are by default sorted so we won't need to sort the input according to the burst time afterwards
     for(int i=0;i<n;i++)
     {
         cout<<"Enter the Burst time : ";
         cin>>m[i];
     }
     sort(m.begin(),m.end());
+    // Declaring a 2D vector of 3 X n. 
+    // The 1st row denotes the Response Time (RT), 2nd row denotes the Wait Time (WT), and the 3rd row denotes the Turn Around Time (TAT)
     vector<vector<int>> v(3,vector<int>(n));
     int count = 0,k=0;
     for(int i=0;i<n;i++)
